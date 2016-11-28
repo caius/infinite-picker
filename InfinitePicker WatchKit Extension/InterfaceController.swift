9be326984e7137a1a8d568172281b812b9bab041
Caius Durling <dev@caius.name>
2016-11-28 01:00:08 +0000

    Basic project

---
+++ InfinitePicker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  InfinitePicker WatchKit Extension
+//
+//  Created by Caius Durling on 28/11/2016.
+//  Copyright © 2016 Caius Durling. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
