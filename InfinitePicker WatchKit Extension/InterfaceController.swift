feeabfdb153bd24acb43ba50f8df393b37f726d7
Caius Durling <dev@caius.name>
2016-11-28 01:13:40 +0000

    Exploring

--- InfinitePicker WatchKit Extension/InterfaceController.swift
+++ InfinitePicker WatchKit Extension/InterfaceController.swift
@@ -7,17 +7,45 @@
 //
 
 import WatchKit
-import Foundation
-
 
 class InterfaceController: WKInterfaceController {
+
+    @IBOutlet var picker: WKInterfacePicker!
+
+    func stringToItem(name: String) -> WKPickerItem {
+        let item = WKPickerItem()
+        item.title = name
+        return item
+    }
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        let pickerItems = [
+            "one",
+            "two",
+            "three",
+        ]
+        picker.setItems(pickerItems.map(self.stringToItem))
+        picker.setSelectedItemIndex(1)
     }
-    
+
+    @IBAction func pickerChanged(_ value: Int) {
+        print("value: \(value)")
+    }
+
+    @IBAction func testTapped() {
+
+        let pickerItems = [
+            "two",
+            "three",
+            "four",
+        ]
+
+        picker.setItems(pickerItems.map(self.stringToItem))
+    }
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
